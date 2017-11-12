ee300c1023b60907d7552ca448a1f56d987cdc09
ahouts <ahouts4@gmail.com>
2017-11-11 21:44:26 -0800

    add build script

---
+++ build-all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+mkdir build dist
+cp -r ./schedule_data.json ./swagger-dist ./build
+
+# linux builds
+env GOOS=linux GOARCH=amd64 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-linux-amd64.zip ./*
+cd ..
+
+env GOOS=linux GOARCH=386 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-linux-i386.zip ./*
+cd ..
+
+env GOOS=linux GOARCH=arm go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-linux-arm.zip ./*
+cd ..
+
+# mac builds
+env GOOS=darwin GOARCH=amd64 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-macOS-amd64.zip ./*
+cd ..
+
+env GOOS=darwin GOARCH=386 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-macOS-i386.zip ./*
+cd ..
+
+# windows builds
+env GOOS=windows GOARCH=amd64 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-windows-amd64.zip ./*
+cd ..
+
+env GOOS=windows GOARCH=386 go build
+cp ./signet-challenge-backend ./build
+cd ./build
+zip ../dist/signet-challenge-backend-windows-i386.zip ./*
+cd ..
